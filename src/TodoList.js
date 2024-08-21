414216a14b962f1be2ab2edf58cca8b130213c76
kim yong hwan <yongdev0273@gmail.com>
2024-08-21 10:43:23 +0900

    Update TodoList.js

--- src/TodoList.js
+++ src/TodoList.js
@@ -28,15 +28,17 @@ export default function TodoList({
     }
   };
   /**데이터에 따라 다른 todo리턴 */
+  const defaultImageUrl = "https://yonghwna.github.io/FEELDO/image/"
   const listComponent = ({ _id, feel, isComplete, text }) => {
     if (isComplete) {
-      return `<li data-id="${_id}" class="todo-item completed"><img class="todoCheck" src="https://yonghwna.github.io/FEELDO/image/circle-check.svg" alt="" />
+      return `<li data-id="${_id}" class="todo-item completed">
+      <img class="todoCheck" src="${defaultImageUrl}circle-check.svg" alt="" />
       <span class="todoText">${text}</span><span class="emozi">${returnFillIcon(
         feel
       )}</span><span class="todoDelete" >🗑️</span></li>`;
     } else {
       return `<li data-id="${_id}" class="todo-item">
-      <img class="todoCheck" src="https://yonghwna.github.io/FEELDO/image/circle.svg" alt="" />
+      <img class="todoCheck" src="${defaultImageUrl}circle.svg" alt="" />
       <span class="todoText">${text}</span><span class="emozi">${returnFillIcon(
         feel
       )}</span><span class="todoDelete" >🗑️</span></li>`;
